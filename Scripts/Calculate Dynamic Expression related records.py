4577722c85907e0ffecc0d9eee4d3cd4711cdb46
roemhildtg <roemhildtg@gmail.com>
2015-11-04 16:13:01 -0600

    Add a header feature. Convert to use da cursor. Separate function.

--- Scripts/Calculate Dynamic Expression related records.py
+++ Scripts/Calculate Dynamic Expression related records.py
@@ -1,50 +1,55 @@
 # Calculates the dynamic expression for a layer and can be used
 # to dynamically display related records text for data driven pages
 # or feature labels
-#
-# Instructions:
+
 # Change the FindLabel parameter to the id field of the main table
 # This id should be related to each of the related tables
-# Also change the first line of FindLabel. Set id equal to the FindLabel parameter
-# Change the formatter functions to match up with what you want displayed
+# Also change the first line of FindLabel. Set parameter equal to the FindLabel parameter
+def FindLabel ( [PARCELID] ):
+    return get_lookup_text([PARCELID])
+
+# create formatter functions to return what you want displayed
 # for each row. These should return plain text for each row.
+
+# returns a string with the parcel zone
+def get_parcel(row):
+    return row["ZONE"]
+
 # change the lookup tables and assign their appropriate formatter function
-#
 # Properties of each lookup item:
 # table: the name of the table in the mxd
 # row_formatter: the function that will format each row's properties and return
 # the text string
 # id_field: the name of the field in the table that is related to the
 # id passed in to FindLabel
-import arcpy
-def get_block(row):
-    return "<BOL>Block {} ({}):\n {} Buildings</BOL>".format(row.Block, row.Reference, row.Number_of_Buildings)
-def get_condition(row):
-    return "- {} Units in {} Condition".format(row.Units, row.Condition)
-def get_type(row):
-    return "- {} Units of {} Type".format(row.Units, row.Type)
+# fields: list of fields by name (optiononal) default '*'
 lookup_tables = [{
-        "table": "Blocks",
-        "row_formatter": get_block,
-        "id_field": "Block"
-    },{
-        "table": "Conditions",
-        "row_formatter": get_condition,
-        "id_field": "Block_Number"
-    }, {
-        "table": "Types",
-        "row_formatter": get_type,
-        "id_field": "Block_Number"
-    }
+    "table": "Related_Zones",
+    "row_formatter": get_parcel,
+    "id_field": "PARCELID",
+    "fields": ['PARCELID', 'ZONE'],
+    "header": 'Parcel Zones:',
+},
+#other lookup tables...
 ]
-def FindLabel ( [Downtown_Parcels_wBlock2.Block_Number] ):
-    id = [Downtown_Parcels_wBlock2.Block_Number]
-    if id:
-        mxd = arcpy.mapping.MapDocument("CURRENT")
-        text = ""
+
+
+def get_lookup_text(id):
+    # init the return value
+    text = ''
+    if id is not None:
         for table in lookup_tables:
-            query = "{} = {}".format(id, table["id_field"])
-            rows = arcpy.SearchCursor(table["table"], query)
-            for row in rows:
-                text ="{}{} \n".format(text, table["row_formatter"](row))
+            if table.has_key('header'):
+                text += "{} \n".format(table['header'])
+            if type(id) is str or type(id) is unicode:
+                query = "{} = '{}'".format(table["id_field"], id)
+            else:
+                query = "{} = {}".format(table["id_field"], id)
+            fields = table['fields'] if table.has_key('fields') else '*'
+            with arcpy.da.SearchCursor(table["table"], fields, where_clause=query) as rows:
+                for row in rows:
+                    dictionary = dict()
+                    for idx, val in enumerate(rows.fields):
+                        dictionary[val] = row[idx]
+                    text += "{} \n".format(table["row_formatter"](dictionary))
     return text
