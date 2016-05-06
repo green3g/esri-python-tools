1c17cdcf40f07158afa4b6fc648c1fd05ca8074e
roemhildtg <roemhildtg@gmail.com>
2016-05-06 11:56:22 -0500

    Move functions around

--- lib/util/File_Operations.py
+++ lib/util/File_Operations.py
@@ -4,6 +4,7 @@ from os import makedirs
 import errno
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from shutil import copyfile
+from arcpy.da import SearchCursor
 
 def make_sure_path_exists(path):
     try:
@@ -90,3 +91,70 @@ def extract_page(input_file, number, output_path, filename_formatter=get_page_fi
             'error while writing output to {}'.format(output_file)
             return False
     return True
+
+def copy_layer_filepath(layer, output_base, fields, log_file=None):
+    """
+    copies files in a path to a new path and splits the required pages into
+    separate files for quicker loading
+        layer:          the path to the layer or layer name in workspace
+        output_base:    the base path to the output file
+        fields:         A list of field names to use. Order should be like this:
+                        [path, name, doc_id, page_num]
+        log_file:       an open file or class with a `write` method
+    """
+    cursor = SearchCursor(layer, fields)
+    print('Scanning layer - {}'.format(layer))
+    if log_file:
+        print 'Writing logfile - {}'.format(log_file)
+        log_file.write('Problem,Layer,Plan,Page\n')
+
+    # a dict to keep track of which files/pages to copy
+    # each key references a file, and the value is a list of pages to copy
+    file_list = {}
+    for row in cursor:
+        folder = row[0]
+        name = row[1]
+        doc_id = row[2]
+        page_num = row[3]
+
+        plan = join(folder, name)
+
+        #make sure the plan isn't already in the dict
+        if not doc_id in file_list:
+            file_list[doc_id] = {
+                'pages': [],
+                'folder': folder,
+                'filename': name
+            }
+
+        #add the page index to the list
+        #page numbers are one more than the page index so we
+        #subtract 1 because the index is one less than the page number
+        try:
+            page_num = int(page_num) - 1
+            if page_num < 0:
+                page_num = 0
+        except:
+            #handle invalid values (null)
+            page_num = 0
+        if not page_num in file_list[doc_id]['pages']:
+            file_list[doc_id]['pages'].append(page_num)
+
+    for plan, props in file_list.iteritems():
+        input_folder = props['folder']
+        pages = props['pages']
+        filename = props['filename']
+        input_file = join(input_folder, filename)
+        output_folder = join(output_base, filename)
+
+        #copy the entire file
+        #log errors
+        if not copy_file(input_folder, output_folder, filename) and log_file:
+            log_file.write('Copy file failed,{},{},{} \n'.format(layer, plan, -99))
+        for page in pages:
+            # and split the pages
+            #log errors for future purposes
+            #functions return false if error occurs
+            if not extract_page(input_file, page, output_folder) and log_file:
+                 log_file.write('Extract page failed,{},{},{} \n'.format(layer, plan, page))
+    print '----Done-----!\n\n'
