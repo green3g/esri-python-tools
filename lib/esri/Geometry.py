a20446adedc1332c941b6a7c378662afe3a9ddbd
roemhildtg <roemhildtg@gmail.com>
2016-06-02 14:25:52 -0500

    Populates output field values from input line fields

--- lib/esri/Geometry.py
+++ lib/esri/Geometry.py
@@ -1,5 +1,5 @@
 from arcpy.da import SearchCursor, InsertCursor
-from arcpy import Describe, AddField_management
+from arcpy import Describe, AddField_management, AddMessage
 
 def polygon_to_point(input_fc, output_fc):
     """
@@ -29,21 +29,37 @@ def line_to_endpoints(input_fc, output_fc, id_field=None):
     feature class
     """
 
+    # id field can either be provided or obtained automagically
     oid_field = id_field if id_field else Describe(input_fc).OIDFieldName
+
+    # we add two custom fields to track the related id and the type of end point
     related_field = 'related_oid'
     type_field = 'point_type'
     AddField_management(output_fc, related_field, 'LONG')
     AddField_management(output_fc, type_field, 'TEXT', field_length=10)
 
-    search_cursor = SearchCursor(input_fc, ['SHAPE@', oid_field])
-    insert_cursor = InsertCursor(output_fc, ["SHAPE@", related_field, type_field])
+
+    fields = [f.name for f in Describe(input_fc).fields if f.name not in ['SHAPE@', 'Shape', 'Shape_Length']]
+    output_fields = fields + ['SHAPE@', related_field, type_field]
+
+    #shape will be the last column selected
+    search_cursor = SearchCursor(input_fc, fields + ['SHAPE@'])
+
+    #our insert cursor
+    insert_cursor = InsertCursor(output_fc, output_fields)
+
+    #identify the spatial reference for projecting the geometry
     spatial_reference = Describe(output_fc).spatialReference
 
     for row in search_cursor:
-        start_point = row[0].projectAs(spatial_reference).firstPoint
-        end_point = row[0].projectAs(spatial_reference).lastPoint
-        oid = row[1]
-        insert_cursor.insertRow([start_point, oid, 'START'])
-        insert_cursor.insertRow([end_point, oid, 'END'])
+        # project and store the geometry
+        geom = row[len(row) - 1].projectAs(spatial_reference)
+
+        # get the row id
+        oid = row[fields.index(oid_field)]
+        
+        # insert two entries but with our new custom geometry
+        insert_cursor.insertRow(row[:-1] + (geom.firstPoint, oid, 'START'))
+        insert_cursor.insertRow(row[:-1] + (geom.lastPoint, oid, 'END'))
 
     del search_cursor, insert_cursor, spatial_reference
